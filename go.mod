7a1ab855c47df156441c78107da80d2e3fdd4ed9
MRegterschot <marijn.regterschot@gmail.com>
2025-02-19 16:01:11 +0100

    feat: update Go version and integrate GbxRemoteGo for event handling

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/MRegterschot/GoController
 
-go 1.23.4
+go 1.24.0
 
-require (
-	alexejk.io/go-xmlrpc v0.5.3
-)
+require alexejk.io/go-xmlrpc v0.5.3
+
+require github.com/MRegterschot/GbxRemoteGo v0.2.0
--- main.go
+++ main.go
@@ -2,20 +2,85 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"alexejk.io/go-xmlrpc"
+	"github.com/MRegterschot/GbxRemoteGo/events"
+	. "github.com/MRegterschot/GbxRemoteGo/gbxclient"
 )
 
 func main() {
-	client, _ := xmlrpc.NewClient("https://bugzilla.mozilla.org/xmlrpc.cgi")
-	defer client.Close()
+	// Create a new GbxClient
+	client := NewGbxClient(Options{})
 
-	result := &struct {
-		BugzillaVersion struct {
-			Version string
+	// Register event handlers
+	onConnectionChan := make(chan interface{})
+	client.Events.On("connect", onConnectionChan)
+	go handleConnect(onConnectionChan)
+
+	onDisconnectChan := make(chan interface{})
+	client.Events.On("disconnect", onDisconnectChan)
+	go handleDisconnect(onDisconnectChan)
+
+	// Connect to the server
+	if err := client.Connect("127.0.0.1", 5000); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := client.SetApiVersion("2023-04-24"); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := client.EnableCallbacks(true); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := client.Authenticate("SuperAdmin", "SuperAdmin"); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Register gbx callback handlers
+	client.OnPlayerConnect = append(client.OnPlayerConnect, func(client *GbxClient, args events.PlayerConnectEventArgs) {
+		fmt.Println("Player connected:", args.Login)
+	})
+
+	client.OnPlayerCheckpoint = append(client.OnPlayerCheckpoint, func(client *GbxClient, args events.PlayerCheckpointEventArgs) {
+		fmt.Println("Player checkpoint:", args)
+	})
+
+	client.OnAnyCallback = append(client.OnAnyCallback, func(client *GbxClient, args CallbackEventArgs) {
+		fmt.Println("Any callback:", args)
+	})
+
+	select {}
+}
+
+func handleConnect(eventChan chan interface{}) {
+	for {
+		select {
+		case event := <-eventChan:
+			if connected, ok := event.(bool); ok {
+				if connected {
+					fmt.Println("Connected")
+				} else {
+					fmt.Println("Not Connected")
+				}
+			} else {
+				fmt.Println("Invalid event type for connect.")
+			}
 		}
-	}{}
+	}
+}
 
-	_ = client.Call("Bugzilla.version", nil, result)
-	fmt.Printf("Version: %s\n", result.BugzillaVersion.Version)
+func handleDisconnect(eventChan chan interface{}) {
+	for {
+		select {
+		case event := <-eventChan:
+			if msg, ok := event.(string); ok {
+				fmt.Println(msg)
+			} else {
+				fmt.Println("Invalid event type for disconnect.")
+			}
+		}
+	}
 }
