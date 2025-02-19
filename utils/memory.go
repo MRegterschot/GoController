266c35c050b2f0dead2d841a4bb73c9ba97d25c2
MRegterschot <marijn.regterschot@gmail.com>
2025-02-19 21:04:34 +0100

    feat: enhance memory logging with runtime tracking and add abs function

--- utils/memory.go
+++ utils/memory.go
@@ -26,21 +26,39 @@ func roundTo2Decimals(val float64) float64 {
 }
 
 // Create log message
-func logMemoryUsage(currentMemoryMB uint64, diffMB uint64) {
-	zap.L().Info(fmt.Sprintf("Memory Usage: Current %.3f MB, Diff %.3f MB", bytesToMB(currentMemoryMB), bytesToMB(diffMB)))
+func logMemoryUsage(currentMemory uint64, diff int64, runtime int) {
+	currentMemoryMB := roundTo2Decimals(bytesToMB(currentMemory))
+	diffMB := roundTo2Decimals(bytesToMB(uint64(abs(diff))))
+
+	sign := "+"
+	if diff < 0 {
+		sign = "-"
+	}
+
+	zap.L().Info(fmt.Sprintf("Memory Usage: Current %.3f MB, Diff %s%.3f MB. Running for %d min.", currentMemoryMB, sign, diffMB, runtime))
+}
+
+// Absolute value function for int64
+func abs(x int64) int64 {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 // Function to start checking memory every minute
 func MemoryChecker() {
+	runtime := 0
 	initialMemory := getMemoryStats()
-	logMemoryUsage(initialMemory, 0)
+	logMemoryUsage(initialMemory, 0, runtime)
 
 	for {
 		time.Sleep(1 * time.Minute)
+		runtime++
 
 		currentMemory := getMemoryStats()
-		diff := currentMemory - initialMemory
+		diff := int64(currentMemory) - int64(initialMemory) // Ensure signed calculation
 
-		logMemoryUsage(currentMemory, diff)
+		logMemoryUsage(currentMemory, diff, runtime)
 	}
 }
