c71f94af8c16bdb0e3fc555da940fecfeed178f7
MRegterschot <marijn.regterschot@gmail.com>
2025-04-02 19:01:07 +0200

    feat: implement record management functionality with CRUD operations in the database

---
+++ database/record.go
@@ -0,0 +1,80 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var collection = "records"
+
+type Record struct {
+	ID    primitive.ObjectID `bson:"_id,omitempty"`
+	Login string             `bson:"login"`
+	Time  int                `bson:"time"`
+
+	CreatedAt primitive.DateTime  `bson:"createdAt"`
+	UpdatedAt primitive.DateTime  `bson:"updatedAt"`
+	DeletedAt *primitive.DateTime `bson:"deletedAt,omitempty"`
+}
+
+func NewRecord(record Record) Record {
+	now := primitive.NewDateTimeFromTime(time.Now())
+
+	newRecord := Record{
+		ID:        primitive.NewObjectID(),
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	CopyRecord(record, &newRecord)
+
+	return newRecord
+}
+
+func CopyRecord(src Record, dest *Record) {
+	dest.Login = src.Login
+	dest.Time = src.Time
+}
+
+func GetRecordByID(ctx context.Context, id primitive.ObjectID) (Record, error) {
+	var record Record
+	err := GetCollection(collection).FindOne(ctx, bson.M{"_id": id}).Decode(&record)
+	return record, err
+}
+
+func GetRecordByLogin(ctx context.Context, login string) (Record, error) {
+	var record Record
+	err := GetCollection(collection).FindOne(ctx, bson.M{"login": login}).Decode(&record)
+	return record, err
+}
+
+func GetRecordsByLogin(ctx context.Context, login string) ([]Record, error) {
+	var records []Record
+	cursor, err := GetCollection(collection).Find(ctx, bson.M{"login": login})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var record Record
+		if err := cursor.Decode(&record); err != nil {
+			return nil, err
+		}
+		records = append(records, record)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func InsertRecord(ctx context.Context, record Record) (*mongo.InsertOneResult, error) {
+	return GetCollection(collection).InsertOne(ctx, record)
+}
